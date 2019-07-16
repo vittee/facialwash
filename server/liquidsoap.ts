c1380fd28b96c63e4948dc023d58ef7f8289310c
Vittee Nakka <vittee@hotmail.com>
2019-07-16 21:45:57 +0700

    Update liquidsoap middleware.

--- server/liquidsoap.ts
+++ server/liquidsoap.ts
@@ -12,17 +12,23 @@ const router = express.Router();
 const handler = new LiquidsoapHandler(router);
 
 router.post('/track', (req, res) => {
-  const meta = req.body;
+  const body = req.body;
 
-  let { sending_time_ms, position_ms } = meta;
+  console.log('Rcvd track', body);
+
+  const meta = _.omit(body, 'frame_duration', 'sending_time_ms', 'position_ms')
+  let { sending_time_ms, position_ms, frame_duration } = body;
 
   sending_time_ms = +sending_time_ms;
   position_ms =  (+position_ms) + (Date.now() - sending_time_ms);
+  frame_duration = +frame_duration;
 
-  handler.emit('track', _.extend(meta, {
+  handler.emit('track', {
+    frame_duration,
     sending_time_ms,
-    position_ms
-  }));
+    position_ms,
+    meta
+  })
 });
 
 export default handler;
