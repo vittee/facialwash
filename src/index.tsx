876328078d3bccb2c5b19b39e57bb11120eda529
Vittee Nakka <vittee@hotmail.com>
2019-07-16 21:49:05 +0700

    Use Overmind.

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import './index.scss';
 import * as serviceWorker from './serviceWorker';
+import { createOvermind } from 'overmind';
+import { config } from 'overminds';
+import { Provider } from 'overmind-react';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const overmind = createOvermind(config);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+ReactDOM.render((
+  <Provider value={overmind}>
+    <App />
+  </Provider>
+), document.getElementById('root'));
+
 serviceWorker.unregister();
